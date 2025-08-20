dd185c4875fed5e1f004bba1da6d27d2393e6b6f
hectorsanchez <hectorsanchez.mov@gmail.com>
2025-08-20 15:51:29 -0300

    evitar colisiones de rutas y controladores, mejor compatibilidad

--- plugin.rb
+++ plugin.rb
@@ -3,50 +3,57 @@
 # version: 0.1
 # authors: Héctor Sanchez
 
-# Plugin básico para consumir la API de Moodle y agrupar usuarios por país
-# Plugin Name: discourse-moodle-users
-# Plugin URL: https://github.com/hectorsanchez/discourse-moodle-users
-
-enabled_site_setting :moodle_api_token
-enabled_site_setting :moodle_api_url
-
 after_initialize do
   module ::DiscourseMoodleUsers
-    class MoodleController < ::ApplicationController
-      def users
-        unless SiteSetting.enabled
-          render json: { error: I18n.t("discourse_moodle_users.disabled") }, status: 403
-          return
-        end
+    PLUGIN_NAME ||= "discourse-moodle-users".freeze
 
-        token = SiteSetting.moodle_api_token
-        moodle_url = SiteSetting.moodle_api_url
-
-        if token.blank? || moodle_url.blank?
-          render json: { error: I18n.t("discourse_moodle_users.invalid_config") }, status: 400
-          return
-        end
-
-        wsfunction = 'core_user_get_users'
-        criteria = 'criteria[0][key]=email&criteria[0][value]=%'
-        format = 'moodlewsrestformat=json'
-        url = "#{moodle_url}?wstoken=#{token}&wsfunction=#{wsfunction}&#{criteria}&#{format}"
-
-        response = Net::HTTP.get(URI(url))
-        users = JSON.parse(response)['users']
-
-        grouped = users.group_by { |u| u['country'] || I18n.t("discourse_moodle_users.no_country") }
-        result = grouped.transform_values do |arr|
-          arr.map { |u| { firstname: u['firstname'], lastname: u['lastname'], email: u['email'] } }
-        end
-
-        render json: { users_by_country: result }
-      end
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace DiscourseMoodleUsers
     end
   end
 
+  # Controlador
+  class ::DiscourseMoodleUsers::MoodleController < ::ApplicationController
+    def users
+      unless SiteSetting.dmu_enabled
+        render json: { error: I18n.t("discourse_moodle_users.disabled") }, status: 403
+        return
+      end
+
+      token = SiteSetting.dmu_moodle_api_token
+      moodle_url = SiteSetting.dmu_moodle_api_url
+
+      if token.blank? || moodle_url.blank?
+        render json: { error: I18n.t("discourse_moodle_users.invalid_config") }, status: 400
+        return
+      end
+
+      wsfunction = 'core_user_get_users'
+      criteria = 'criteria[0][key]=email&criteria[0][value]=%'
+      format = 'moodlewsrestformat=json'
+      url = "#{moodle_url}?wstoken=#{token}&wsfunction=#{wsfunction}&#{criteria}&#{format}"
+
+      response = Net::HTTP.get(URI(url))
+      users = JSON.parse(response)['users']
+
+      grouped = users.group_by { |u| u['country'] || I18n.t("discourse_moodle_users.no_country") }
+      result = grouped.transform_values do |arr|
+        arr.map { |u| { firstname: u['firstname'], lastname: u['lastname'], email: u['email'] } }
+      end
+
+      render json: { users_by_country: result }
+    end
+  end
+
+  # Rutas dentro del Engine
+  DiscourseMoodleUsers::Engine.routes.draw do
+    get '/users' => 'moodle#users'
+  end
+
+  # Montar el Engine en la app principal
   Discourse::Application.routes.append do
-    get '/moodle/users' => 'discourse_moodle_users/moodle#users'
+    mount ::DiscourseMoodleUsers::Engine, at: '/moodle'
   end
 end
 
